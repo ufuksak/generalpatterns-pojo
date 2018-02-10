03efed266e28e9038837fa9a81e1c6f381300c95
Andrey Artyukhov <andrey.artyukhov@aurea.com>
2018-02-10 14:54:27 +0700

    Simple reporting

---
+++ src/main/java/com/aurea/testgenerator/coverage/TypeCoverageQuery.groovy
@@ -0,0 +1,17 @@
+package com.aurea.testgenerator.coverage
+
+import com.aurea.testgenerator.source.Unit
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.ast.body.TypeDeclaration
+import groovy.transform.Immutable
+
+@Immutable(knownImmutableClasses = [Unit.class, TypeDeclaration.class])
+class TypeCoverageQuery {
+    Unit unit
+    TypeDeclaration type
+    int anonymousClassIndex
+
+    static TypeCoverageQuery of(Unit unit, TypeDeclaration type) {
+        new TypeCoverageQuery(unit, type, 0)
+    }
+}
