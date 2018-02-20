8496fb597d5920f27761827fd4729c7847c33b7c
Andrey Artyukhov <andrey.artyukhov@aurea.com>
2018-02-20 16:33:14 +0700

    Sanity checks

---
+++ src/main/java/com/aurea/testgenerator/source/SanityChecker.groovy
@@ -0,0 +1,51 @@
+package com.aurea.testgenerator.source
+
+import com.aurea.testgenerator.source.structure.Module
+import com.aurea.testgenerator.source.structure.Project
+import com.esotericsoftware.yamlbeans.YamlReader
+import groovy.util.logging.Log4j2
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+
+@Log4j2
+class SanityChecker {
+
+    static void main(String[] args) {
+        new File("D:/trilogy-group-only-java").eachDir { dir ->
+            if (dir.name == 'acr-aurea-ace-butterfly') {
+                YamlReader reader = new YamlReader(new FileReader(dir.toPath().resolve('project-structure.yml').toFile()))
+                reader.getConfig().setClassTag('Project', Project)
+                reader.getConfig().setClassTag('Module', Module)
+                Project project = reader.read(Project)
+                reader.close()
+                if (project.modules) {
+                    log.info "Generating for project $dir"
+                    project.modules.each {
+                        String src = it.src.replace('trilogy-group-java', 'D:/trilogy-group-only-java')
+                        String projectSrc = "--project.src=${src}"
+                        Path pathToSrc = Paths.get(src)
+                        Path pathToTest = pathToSrc.subpath(0, pathToSrc.nameCount - 1).resolve('test')
+                        String projectOut = "--project.out=$pathToTest"
+                        ProcessBuilder builder = new ProcessBuilder(
+                                "java", "-jar", "D:/crossover/repos/BigCodeTestGenerator/build/libs/big-code-1.2.1.jar",
+                                projectSrc, projectOut, "--spring.profiles.active=open-pojo,class-level")
+                        builder.inheritIO()
+                        log.info """
+----------------------------------------------------------------------------------------------------------------------------------------------------
+
+${builder.command().join(" ")} 
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+
+                    """
+                        Process started = builder.start()
+                        started.waitFor()
+                        println "Finished generation for $it: ${started.exitValue()}"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/aurea/testgenerator/source/SourceLocator.groovy
@@ -0,0 +1,95 @@
+package com.aurea.testgenerator.source
+
+import com.aurea.testgenerator.config.ProjectConfiguration
+import com.aurea.testgenerator.source.structure.Module
+import com.aurea.testgenerator.source.structure.Project
+import com.esotericsoftware.yamlbeans.YamlWriter
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ParseProblemException
+import com.github.javaparser.ast.CompilationUnit
+import groovy.util.logging.Log4j2
+
+import java.nio.file.Path
+
+@Log4j2
+class SourceLocator {
+
+    static void main(String[] args) {
+        List<File> unstructuredProjects = []
+        new File("D:/trilogy-group-java").eachFile {
+            if (it.directory) {
+                if (!it.toPath().resolve('project-structure.yml').toFile().exists()) {
+                    unstructuredProjects << it
+                }
+            }
+        }
+        unstructuredProjects.parallelStream().each {
+            write it.absolutePath
+        }
+    }
+
+    private static void write(String projectName) {
+        ProjectConfiguration cfg = new ProjectConfiguration(
+                src: projectName
+        )
+        Set<Path> rootPaths = []
+        new JavaSourceFinder(cfg).javaClasses().each { javaClassPath ->
+            try {
+                CompilationUnit cu = JavaParser.parse(javaClassPath)
+                String packageName = cu.packageDeclaration.map { it.nameAsString }.orElse("")
+                if (!packageName) {
+                    log.error "No package declaration in $javaClassPath"
+                } else {
+                    Path packagePath = PathUtils.packageNameToPath(packageName)
+                    Path expectedPackagePath = javaClassPath.subpath(0, javaClassPath.nameCount - 1)
+                    if (!expectedPackagePath.endsWith(packagePath)) {
+                        log.error "Inconsistent java class path and package declaration: $expectedPackagePath does not end with $packagePath"
+                    } else {
+                        int nameToSubstract = packagePath.nameCount
+                        Path rootPath = expectedPackagePath.subpath(0, javaClassPath.nameCount - nameToSubstract - 1)
+                        rootPaths << rootPath
+                    }
+                }
+
+            } catch (ParseProblemException ppe) {
+                log.error "Failed to parse $javaClassPath"
+            }
+        }
+
+        Map<Path, Path> testSrcs = new HashMap<>()
+        Map<Path, Path> testBeforeLastSrcs = new HashMap<>()
+        Set<Path> srcs = []
+        List<String> testDirectories = ['test', 'tests', 'integrationTest', 'Test', 'itest', 'testsrc', 'src_test', 'unittests']
+        rootPaths.each {
+            String lastName = it.getName(it.nameCount - 1)
+            String beforeLastname = it.getName(it.nameCount - 2)
+            if (lastName in testDirectories) {
+                testSrcs.put(it.subpath(0, it.nameCount - 1), it)
+            } else if (beforeLastname in testDirectories) {
+                testBeforeLastSrcs.put(it.subpath(0, it.nameCount - 2), it)
+            } else {
+                srcs << it
+            }
+        }
+
+        List<Module> modules = srcs.collect {
+            Path commonPathWithTests = it.subpath(0, it.nameCount - 1)
+            Path commonPathWithBeforeLastTests = it.subpath(0, it.nameCount - 2)
+            Path testPath = null
+            if (testSrcs.containsKey(commonPathWithTests)) {
+                testPath = testSrcs[commonPathWithTests]
+            } else if (testBeforeLastSrcs.containsKey(commonPathWithBeforeLastTests)) {
+                testPath = testBeforeLastSrcs[commonPathWithBeforeLastTests]
+            }
+
+            return new Module(it.toString(), testPath.toString())
+        }
+
+        Project project = new Project(modules)
+        YamlWriter writer = new YamlWriter(new FileWriter("${projectName}/project-structure.yml"))
+        writer.getConfig().setClassTag('Project', Project)
+        writer.getConfig().setClassTag('Module', Module)
+        writer.write(project)
+        writer.close()
+    }
+}
