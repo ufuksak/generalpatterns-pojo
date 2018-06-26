79cc97468f909953361b162efcf4b671e396a11b
Fernando Santos <fernandopinadossantos@gmail.com>
2018-06-26 19:58:47 +0100

    Imports are now fixed automatically

--- src/main/java/com/aurea/testgenerator/source/OverrideClassWriteStrategy.groovy
+++ src/main/java/com/aurea/testgenerator/source/OverrideClassWriteStrategy.groovy
@@ -1,9 +1,9 @@
 package com.aurea.testgenerator.source
 
-import com.aurea.common.ImportHelper
+import com.google.googlejavaformat.java.ImportOrderer
+import com.google.googlejavaformat.java.RemoveUnusedImports
 import groovy.util.logging.Log4j2
 import org.springframework.stereotype.Component
-
 
 @Log4j2
 @Component
@@ -12,7 +12,10 @@ class OverrideClassWriteStrategy implements ExistingTestClassWriteStrategy {
     void write(File existingTest, Unit testUnit) {
         log.debug "Deleting $existingTest"
         existingTest.delete()
-        existingTest.write(ImportHelper.organizeImports(testUnit.cu.toString()))
+        String fileContents = testUnit.cu.toString()
+        fileContents = RemoveUnusedImports.removeUnusedImports(fileContents)
+        fileContents = ImportOrderer.reorderImports(fileContents)
+        existingTest.write(fileContents)
     }
 
     @Override
--- src/main/java/com/aurea/testgenerator/source/UnitTestWriter.groovy
+++ src/main/java/com/aurea/testgenerator/source/UnitTestWriter.groovy
@@ -2,6 +2,8 @@ package com.aurea.testgenerator.source
 
 import com.aurea.common.ImportHelper
 import com.aurea.testgenerator.config.ProjectConfiguration
+import com.google.googlejavaformat.java.ImportOrderer
+import com.google.googlejavaformat.java.RemoveUnusedImports
 import groovy.util.logging.Log4j2
 import one.util.streamex.StreamEx
 import org.springframework.beans.factory.annotation.Autowired
@@ -42,7 +44,10 @@ class UnitTestWriter {
             }
             if (!testFile.exists()) {
                 log.debug "Writing test: $testFile"
-                testFile.write(ImportHelper.organizeImports(unit.cu.toString()))
+                String fileContents = unit.cu.toString()
+                fileContents = RemoveUnusedImports.removeUnusedImports(fileContents)
+                fileContents = ImportOrderer.reorderImports(fileContents)
+                testFile.write(fileContents)
             } else {
                 existingTestClassWriteStrategy.write(testFile, unit)
             }
