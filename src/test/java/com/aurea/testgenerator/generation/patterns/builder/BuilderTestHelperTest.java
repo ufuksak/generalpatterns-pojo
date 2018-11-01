cf605c30a10a99705f1e8489bd134e2e5b2b85ab
Maciste Saldanha <maciste.saldanha@aurea.com>
2018-11-01 10:51:05 +0300

    Added more tests.

--- src/test/java/com/aurea/testgenerator/generation/patterns/builder/BuilderTestHelperTest.java
+++ src/test/java/com/aurea/testgenerator/generation/patterns/builder/BuilderTestHelperTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Test;
@@ -40,6 +41,11 @@ public class BuilderTestHelperTest {
             + "    private String lastName;\n"
             + "\n"
             + "    public PersonBuilder firstName(String firstName) {\n"
+            + "        this.firstName = firstName;\n"
+            + "        return this;\n"
+            + "    }\n"
+            + "\n"
+            + "    public PersonBuilder withFirstName(String firstName) {\n"
             + "        this.firstName = firstName;\n"
             + "        return this;\n"
             + "    }\n"
@@ -133,17 +139,20 @@ public class BuilderTestHelperTest {
 
     @Test
     public void givenAClassWithBuilderSuffixAndBuilderMethodWhenIsBuilderIsCalledThenItShouldReturnTrue() {
+        // Arrange
         CompilationUnit cu = JavaParser.parse(JAVA_CODE);
 
         List<ClassOrInterfaceDeclaration> classes = cu.findAll(ClassOrInterfaceDeclaration.class)
                 .stream().filter(node -> !node.isInterface() && node.getNameAsString().equals("PersonBuilder"))
                 .collect(Collectors.toList());
 
+        // Act/Assert
         assertThat(BuilderTestHelper.isBuilder(classes.get(0))).isTrue();
     }
 
     @Test
     public void givenAClassWithoutBuilderSuffixWhenIsBuilderIsCalledThenItShouldReturnFalse() {
+        // Arrange
         CompilationUnit cu = JavaParser.parse(JAVA_CODE);
 
         List<ClassOrInterfaceDeclaration> classes = cu.findAll(ClassOrInterfaceDeclaration.class)
@@ -151,11 +160,13 @@ public class BuilderTestHelperTest {
                         && node.getNameAsString().equals("SomeBuilderWithNoBuilderSuffix"))
                 .collect(Collectors.toList());
 
+        // Act/Assert
         assertThat(BuilderTestHelper.isBuilder(classes.get(0))).isFalse();
     }
 
     @Test
     public void givenAClassWithoutBuildMethodWhenIsBuilderIsCalledThenItShouldReturnFalse() {
+        // Arrange
         CompilationUnit cu = JavaParser.parse(JAVA_CODE);
 
         List<ClassOrInterfaceDeclaration> classes = cu.findAll(ClassOrInterfaceDeclaration.class)
@@ -163,25 +174,61 @@ public class BuilderTestHelperTest {
                         && node.getNameAsString().equals("SomeClassWithNoBuildMethodBuilder"))
                 .collect(Collectors.toList());
 
+        // Act/Assert
         assertThat(BuilderTestHelper.isBuilder(classes.get(0))).isFalse();
     }
 
     @Test
     public void givenAnEmptyTextWhenFirstToUpperCaseIsCalledThenItShouldReturnTheSameValue() {
+        // Arrange
         String text = "";
+
+        // Act/Assert
         assertThat(BuilderTestHelper.firstToUpperCase(text)).isEqualTo(text);
     }
 
     @Test
     public void givenAnATextWithLen1WhenFirstToUpperCaseIsCalledThenItShouldReturnItUppercased() {
+        // Arrange
         String text = "c";
+
+        // Act/Assert
         assertThat(BuilderTestHelper.firstToUpperCase(text)).isEqualTo("C");
     }
 
     @Test
     public void givenAnATextWhenFirstToUpperCaseIsCalledThenItShouldReturnItCapitalized() {
+        // Arrange
         String text = "upperCase";
+
+        // Act/Assert
         assertThat(BuilderTestHelper.firstToUpperCase(text)).isEqualTo("UpperCase");
+    }
+
+    @Test
+    public void givenAMethodWhenBuildGetterNameIsCalledThenItShouldReturnGetterName() {
+        // Arrange
+        CompilationUnit cu = JavaParser.parse(JAVA_CODE);
+
+        List<MethodDeclaration> classes = cu.findAll(MethodDeclaration.class)
+                .stream().filter(node -> node.getNameAsString().equals("firstName"))
+                .collect(Collectors.toList());
+
+        // Act/Assert
+        assertThat(BuilderTestHelper.buildGetterName("get", classes.get(0))).isEqualTo("getFirstName");
+    }
+
+    @Test
+    public void givenAMethodStartingWithWithWhenBuildGetterNameIsCalledThenItShouldReturnGetterName() {
+        // Arrange
+        CompilationUnit cu = JavaParser.parse(JAVA_CODE);
+
+        List<MethodDeclaration> classes = cu.findAll(MethodDeclaration.class)
+                .stream().filter(node -> node.getNameAsString().equals("withFirstName"))
+                .collect(Collectors.toList());
+
+        // Act/Assert
+        assertThat(BuilderTestHelper.buildGetterName("get", classes.get(0))).isEqualTo("getFirstName");
     }
 
 }
